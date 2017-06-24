c28215fa1109b2e38c356689499887d1f3344feb
smile-smile <1371826591@qq.com>
2017-06-24 15:30:59 +0800

    delete ecommerce

--- e_commerce_site_crawler/e_commerce_site_crawler/ecommerce/spiderEntiy/category_item.py
+++
@@ -1,5 +0,0 @@
-class category_item:
-    def __init__(self,label,children_list,is_bottom):
-        self.label = label
-        self.children_list = children_list
-        self.is_bottom = is_bottom
